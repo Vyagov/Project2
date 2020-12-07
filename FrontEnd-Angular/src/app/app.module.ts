529088d4ca1e7ca54f430398c15c02f590d1376b
Vyagov <ivanviagov@abv.bg>
2020-12-07 03:23:19 +0200

    create home and not-found component

--- FrontEnd-Angular/src/app/app.module.ts
+++ FrontEnd-Angular/src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import {CoreModule} from './core/core.module';
+import {NavigationComponent} from './core/navigation/navigation.component';
+import {FooterComponent} from './core/footer/footer.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    NavigationComponent,
+    FooterComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {
+}
